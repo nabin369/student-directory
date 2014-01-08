59550f9fd1d87be45141c00d9e1058e635d742e3
nabin369 <nabinrai369@gmail.com>
2014-01-08 15:46:29 +0000

    used nested array and hashes

--- directory.rb
+++ directory.rb
@@ -1,18 +1,18 @@
 
 # array
 students = [ 
-			"Biwek Shrestha",
-			"Nabin Rai",
-			"Andrey",
-			"Makis Otman",
-			"James Hunter",
-			"Roy",
-			"Peter",
-			"Jorja",
-			"Rolando",
-			"Abraham Thomas",
-			"Berta Gutierrez",
-			"Ayaz"
+			{:names => "Biwek Shrestha",  :cohort =>:january},
+			{:names =>"Nabin Rai",  :cohort => :january},
+			{:names => "Andrey",  :cohort => :january},
+			{:names => "Makis Otman",  :cohort => :january},
+			{:names => "James Hunter",  :cohort => :january},
+			{:names => "Roy",  :cohort => :january},
+			{:names => "Peter",  :cohort => :january},
+			{:names => "Jorja",  :cohort => :january},
+			{:names => "Rolando",  :cohort => :january},
+			{:names => "Abraham Thomas",  :cohort => :january},
+			{:names => "Berta Gutierrez", :cohort =>  :january},
+			{:names => "Ayaz", :cohort => :january}
 			]
 
 # 1. print list of students  
@@ -22,10 +22,10 @@ def print_header
 	puts "----------------------------------------"
 end
 
-# Method for 
+# Method for printing student names
 def print(arr_name)
 	arr_name.each do |stu|
-		puts stu
+		puts "#{stu[:names]} (#{stu[:cohort]} cohort)" 
 	end
 end
 
@@ -35,6 +35,6 @@ def print_footer(students)
 end
 
 # Prints header, students names, and footer
-puts print_header
-puts print(students)
-puts print_footer(students)
+print_header
+print(students)
+print_footer(students)
