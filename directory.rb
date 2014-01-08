a66772549f8a442586a2fd3d992ba4ef625b5abf
nabin369 <nabinrai369@gmail.com>
2014-01-08 15:01:57 +0000

    added arrays and made use of iteration

--- directory.rb
+++ directory.rb
@@ -1,21 +1,28 @@
-# assign a variable
-students_count = 12
+
+# array
+students = [ 
+			"Biwek Shrestha",
+			"Nabin Rai",
+			"Andrey",
+			"Makis Otman",
+			"James Hunter",
+			"Roy",
+			"Peter",
+			"Jorja",
+			"Rolando",
+			"Abraham Thomas",
+			"Berta Gutierrez",
+			"Ayaz"
+			]
 
 # 1. print list of students  
 puts "----------------------------------------"
 puts "Makers Academy - January Cohort Students"
 puts "----------------------------------------"
-puts "Nabin Rai"
-puts "Biwek Shrestha"
-puts "Andrey"
-puts "Makis Otman"
-puts "James Hunter"
-puts "Roy"
-puts "Peter"
-puts "Jorja"
-puts "Rolando"
-puts "Abraham Thomas"
-puts "Berta Gutierrez"
-puts "Ayaz"
+
+students.each do |stu|
+	puts stu
+end
+
 # 3. Then we print the overall number of students
-puts "Overall we have #{students_count} students"
+puts "Overall we have #{students.length} students"
